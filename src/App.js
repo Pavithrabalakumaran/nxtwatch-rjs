18186c1796e2660a0c4732885c19b596eeb51b4b
Pavithrabalakumaran <pavithrachandrasekar9360@gmail.com>
2024-01-08 06:45:16 +0000

    nxt2

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
+import ThemeContext from './context/ThemeContext'
+
+import './App.css'
+
+// Replace your code here
+class App extends Component {
+  state = {isDarkTheme: false, savedVideosList: []}
+
+  toggleTheme = () => {
+    this.setState(prevState => ({
+      isDarkTheme: !prevState.isDarkTheme,
+    }))
+  }
+
+  /*
+  toggleTabId = id => {
+    this.setState({activeTabId: id})
+  }
+  */
+
+  saveVideoButtonClicked = data => {
+    const {savedVideosList} = this.state
+    this.setState({savedVideosList: [...savedVideosList, data]})
+  }
+
+  render() {
+    const {isDarkTheme, savedVideosList} = this.state
+
+    return (
+      <ThemeContext.Provider
+        value={{
+          isDarkTheme,
+          toggleTheme: this.toggleTheme,
+          saveVideoButtonClicked: this.saveVideoButtonClicked,
+          savedVideosList,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </ThemeContext.Provider>
+    )
+  }
+}
+
+export default App
