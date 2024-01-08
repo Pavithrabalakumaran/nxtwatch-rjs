c451d824dea44af502ca1fc7fe515c843e23e94a
Pavithrabalakumaran <pavithrachandrasekar9360@gmail.com>
2024-01-08 18:48:21 +0000

    nxt4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {Component} from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
 import ThemeContext from './context/ThemeContext'
 
 import './App.css'
@@ -41,6 +42,7 @@ class App extends Component {
       >
         <Switch>
           <Route exact path="/login" component={Login} />
+          <ProtectedRoute exact path="/" component={Home} />
         </Switch>
       </ThemeContext.Provider>
     )
